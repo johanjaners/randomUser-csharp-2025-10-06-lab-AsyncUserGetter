1166092376d50962d53d54e37ef424772039f3ab
Johan Janérs <johanjaners@gmail.com>
2025-10-20 11:24:50 +0200

    Update GetResultsFromApi method to return Results

--- GROAN.Business/RandomUserClient.cs
+++ GROAN.Business/RandomUserClient.cs
@@ -5,14 +5,14 @@ namespace GROAN.Business;
 public class RandomUserClient
 {
     
-    public async Task<RandomUserResponse> GetResultsFromApi()
+    public async Task<List<Result>> GetResultsFromApi()
     {
         var client = new HttpClient();
         var url = "https://randomuser.me/api/?results=50&inc=name,location,phone,nat";
         var json = await client.GetStringAsync(url);
 
-        var data = JsonSerializer.Deserialize<RandomUserResponse>(json);
-        
-        return data;
+        var response = JsonSerializer.Deserialize<RandomUserResponse>(json);
+
+        return response.Results;
     }
 }
