108211099d790092de49a92f83854a3b536fd42d
Johan Janérs <johanjaners@gmail.com>
2025-10-20 10:12:42 +0200

    Add class RandomUserResponse

---
+++ GROAN.Business/Models/RandomUserResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
+
+namespace GROAN.Business;
+
+public class RandomUserResponse
+{
+    [JsonPropertyName("results")]
+    public List<Results> Results { get; set; } = new();
+}
