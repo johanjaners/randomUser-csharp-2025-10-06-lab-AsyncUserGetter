8531ba3e7f80fd4fd75013c2b99c6a161635e073
Johan Janérs <johanjaners@gmail.com>
2025-10-20 13:54:46 +0200

    Add search function for filter of last name

--- GROAN.App/Program.cs
+++ GROAN.App/Program.cs
@@ -7,12 +7,43 @@ foreach (var arg in args)
 }
 
 var client = new RandomUserClient();
-var results = await client.GetResultsFromApi();
+var people = await client.GetResultsFromApi();
 
-foreach (var r in results)
-{
-  Console.WriteLine("First name: " + r.Name.First);
-  Console.WriteLine("City: " + r.Location.City);
-}
+// foreach (var person in people)
+// {
+//   Console.WriteLine($"Name: {person.Name.First} {person.Name.Last}");
+//   Console.WriteLine("City: " + person.Location.City);
+//   Console.WriteLine("Country: " + person.Location.Country);
+//   Console.WriteLine();
+// }
 
-Console.WriteLine("No of persons: " + results.Count);
+Console.WriteLine("No of people: " + people.Count);
+
+var peopleGroupedByCountry = people
+  .GroupBy(person => person.Location.Country);
+
+Console.WriteLine("No of countries: " + peopleGroupedByCountry.Count());
+
+// foreach(var countryGroup in peopleGroupedByCountry)
+// {
+//   Console.WriteLine("Country: " + countryGroup.Key);
+//   foreach (var person in countryGroup)
+//   {
+//     Console.WriteLine($"Name: {person.Name.First} {person.Name.Last}");
+//   }
+//   Console.WriteLine();
+// }
+
+
+string? lastNameForFilter = "";
+
+if (args.Length > 0)
+  lastNameForFilter = args[0];
+
+var filteredPeople = people
+  .Where(p => p.Name.Last.Contains(lastNameForFilter))
+  .ToList();
+
+Console.WriteLine("Found people by filter on last name: " + filteredPeople.Count);
+foreach (var person in filteredPeople)
+  Console.WriteLine($"{person.Name.First} {person.Name.Last}");
