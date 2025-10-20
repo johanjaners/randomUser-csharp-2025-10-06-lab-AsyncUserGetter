56f4c0512b0af4e1cd68627b087f94e4b4f22e69
Johan Janérs <johanjaners@gmail.com>
2025-10-20 11:25:52 +0200

    Update GROAN.App program with foreach to write each persons name

--- GROAN.App/Program.cs
+++ GROAN.App/Program.cs
@@ -1,5 +1,13 @@
-﻿Console.WriteLine("here are the arguments");
+﻿using GROAN.Business;
+
+Console.WriteLine("here are the arguments");
 foreach (var arg in args)
 {
   Console.WriteLine($"{arg}");
 }
+
+var client = new RandomUserClient();
+var results = await client.GetResultsFromApi();
+
+foreach (var r in results)
+  Console.WriteLine($"{r.Name.First}");
